ebc30b184ec8f17bfd074ab05a6f365d44fc0d55
João Antunes <joao.antunes@efabrika.com.br>
2021-08-04 11:30:16 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Aylan from './assets/Ursinho.png';
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+      <div className="App-box">
+        <img src={Aylan} className="App-logo" alt="logo" />
+        <p>
+          Alou Mundão!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
